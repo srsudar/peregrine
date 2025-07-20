0efc6fb5f2fb885ea55f3f8b0de303906f0dcbbb
Sam Sudar <sudar.sam@gmail.com>
2025-07-20 12:26:38 +0100

    clean up a bit, fix errors

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,29 +1,27 @@
-'use strict';
+"use strict";
 
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
-chrome.omnibox.onInputChanged.addListener(
-  async function(text, suggest) {
-    const result = await chrome.storage.local.get(text);
-    const url = result[text];
-    
-    if(url == undefined) {
-        chrome.omnibox.setDefaultSuggestion({"description" : "No redirect found."});
-    } else {
-        chrome.omnibox.setDefaultSuggestion({"description" : "Redirecting you to: " + url});
-    }
+chrome.omnibox.onInputChanged.addListener(async function (text, suggest) {
+  const result = await chrome.storage.sync.get(text);
+  const url = result[text];
+
+  if (url == undefined) {
+    chrome.omnibox.setDefaultSuggestion({ description: "No redirect found." });
+  } else {
+    chrome.omnibox.setDefaultSuggestion({
+      description: "Redirecting you to: " + url,
+    });
   }
-);
+});
 
 // This event is fired when the user accepts the input in the omnibox.
-// It opens the redirect that the user-given key maps to. 
-chrome.omnibox.onInputEntered.addListener(
-  async function(text) {
-    const result = await chrome.storage.local.get(text);
-    const urlval = result[text];
-    console.log("url: " + urlval);
-    if(urlval != undefined) { 
-        chrome.tabs.update({ url: urlval});   
-    }
+// It opens the redirect that the user-given key maps to.
+chrome.omnibox.onInputEntered.addListener(async function (text) {
+  const result = await chrome.storage.sync.get(text);
+  const urlval = result[text];
+  console.log("url: " + urlval);
+  if (urlval != undefined) {
+    chrome.tabs.update({ url: urlval });
   }
-);
+});
--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -1,36 +1,43 @@
-'use strict';
+"use strict";
 
-// This script contains logic for the settings page. It allows users to 
-// create new directs and delete ones. 
+// This script contains logic for the settings page. It allows users to
+// create new directs and delete ones.
 
-// When the user specifies a key and redirect, it's saved. 
-var createRedirect = async function() {
+// When the user specifies a key and redirect, it's saved.
+var createRedirect = async function () {
   var given_key = document.getElementById("newInput").value;
   var redirect = document.getElementById("url").value;
-  if (redirect.substring(0, 7) != "http://" && redirect.substring(0, 8) != "https://") {
+  if (
+    redirect.substring(0, 7) != "http://" &&
+    redirect.substring(0, 8) != "https://"
+  ) {
     // TODO: There may be a better way of doing this. Regex?
-    redirect = "http://".concat(redirect);  
+    redirect = "http://".concat(redirect);
   }
 
   // TODO: factor out this code
-  if(given_key != undefined && given_key != "" && given_key != " ") {
-    document.getElementById("confirmationVal").innerHTML = "Your Redirect was created! <br />" +
-      "<b>" + given_key + "</b>" + " --> " + redirect; 
-    
-    // Save to chrome.storage.local instead of localStorage
+  if (given_key != undefined && given_key != "" && given_key != " ") {
+    document.getElementById("confirmationVal").innerHTML =
+      "Your Redirect was created! <br />" +
+      "<b>" +
+      given_key +
+      "</b>" +
+      " --> " +
+      redirect;
+
     const data = {};
     data[given_key] = redirect;
-    await chrome.storage.local.set(data);
-  }  
-}
+    await chrome.storage.sync.set(data);
+  }
+};
 
 // Displays saved redirects in a table.
-window.onload = async function() {
+window.onload = async function () {
   var table = document.getElementById("mainTables");
 
   // Get all stored redirects
-  const allRedirects = await chrome.storage.local.get(null);
-  
+  const allRedirects = await chrome.storage.sync.get(null);
+
   for (var key in allRedirects) {
     // Creates an empty table row and adds it to the first position of the table
     var row = table.insertRow(-1);
@@ -42,24 +49,30 @@ window.onload = async function() {
 
     // Populates the new cells with text and a delete button
     cell1.innerHTML = key;
-    cell2.innerHTML = allRedirects[key]; 
-    cell3.innerHTML = "<button id='" + key + "' class='removeElement'>Remove</button>" 
+    cell2.innerHTML = allRedirects[key];
+    cell3.innerHTML =
+      "<button id='" + key + "' class='removeElement'>Remove</button>";
   }
 
   var items = document.getElementsByClassName("removeElement");
   for (var i = 0; i < items.length; i++) {
-    items[i].onclick = function() {removeRedirect(this);}
-  } 
-}
+    items[i].onclick = function () {
+      removeRedirect(this);
+    };
+  }
+};
 
 // Removes the key, redirect, and row in the table and refreshes the page so that
 // the table of redirects is updated.
-var removeRedirect = async function(button) {  
-    var key = button.id;
-    console.log("Deleting " + button.id); 
-    await chrome.storage.local.remove(key);
-    chrome.tabs.reload();
-}
+var removeRedirect = async function (button) {
+  var key = button.id;
+  console.log("Deleting " + button.id);
+  await chrome.storage.sync.remove(key);
+  chrome.tabs.reload();
+};
 
-var button = document.getElementById("new").onclick = createRedirect;
-var remove = document.getElementsByClassName("removeElement").onclick = function () {gallerymake();};
+var button = (document.getElementById("new").onclick = createRedirect);
+var remove = (document.getElementsByClassName("removeElement").onclick =
+  function () {
+    gallerymake();
+  });
